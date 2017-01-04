bab796dbb1767bf190dd1ff7d258bfae7e76c1f6
Joël Gaujard <joel@depiltech.com>
2017-01-04 23:55:17 +0100

    Rename configuration keys

--- config/mailtrap.php
+++ config/mailtrap.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['mailtrap_api_key'] = 'bd35f89bc30c5f4db5f43c3953da19bd';
+$config['mailtrap_api_token'] = 'bd35f89bc30c5f4db5f43c3953da19bd';
 
-$config['debug'] = FALSE;
+$config['mailtrap_debug'] = FALSE;
